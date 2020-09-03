651d3056273cc120462c3e3928be86a3903dded6
unknown <pifei@hollysys.net>
2020-09-03 20:11:16 +0800

    提交代码

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+import 'ant-design-vue/dist/antd.less'
+import Antd from 'ant-design-vue'
+Vue.config.productionTip = false
+Vue.use(Antd)
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
